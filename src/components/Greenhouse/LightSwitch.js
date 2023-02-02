1b805c8cb2306825f9c24b874eb5414a8d5c8460
Sean Baeyens <srbaeyens@gmail.com>
2023-02-02 15:28:08 -0600

    phases 1 & 2

--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,8 +1,17 @@
 import './LightSwitch.css';
+import { useTheme } from '../../context/ThemeContext'
 
 function LightSwitch() {
+  const { themeName, setThemeName } = useTheme()
+    console.log(useTheme());
+  // themeName = "day"
+  function changeLight(e) {
+    if (themeName === "day") setThemeName('night')
+    if (themeName === 'night') setThemeName('day')
+  }
+
   return (
-    <div className="light-switch day">
+    <div onClick={ changeLight} className={ `light-switch ${themeName}`} >
       <div className="on">DAY</div>
       <div className="off">NIGHT</div>
     </div>
